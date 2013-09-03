ee328f6611689d8d87cc4834ad5f6987025c4c06
Yi Zhang <beingzy@users.noreply.github.com>
2013-09-03 10:30:50 -0400

    Update db_connection.php

--- php/db/db_connection.php
+++ php/db/db_connection.php
@@ -2,10 +2,10 @@
 // MySQL database connection
 session_start();
 //if($_SESSION['login']){
-$db_hostname = "localhost";
-$db_username = "root";
-$db_password = "8233156";
-$db_database = "mockup_db";
+$db_hostname = "localhost"; // for local database on the server
+$db_username = "username"; // database user name, mysql default username is root
+$db_password = "password"; // password
+$db_database = "database"; // database for virtual company
 
 $db = new mysqli($db_hostname, $db_username, $db_password, $db_database);
 if($db->connect_errno > 0) die("Unable to connect to database: ". $db->connect_error);
